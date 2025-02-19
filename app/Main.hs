f0e7a9c62b88b457f1a067a3287bdbbb91855be9
unknown <zhanglei@hslearn.cn>
2025-02-19 22:36:04 +0800

    parse dns type

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,25 @@ import Serialization
 import Data.Binary 
 import Data.Binary.Get
 import Types
+  {-
+        5 -> CNAME
+        6 -> SOA
+        12 -> PTR
+        15 -> MX
+        16 -> TXT
+        28 -> AAAA
+        _ -> Other typ
+        -}
+
+parseAllType :: BS.ByteString -> IO()
+parseAllType input = case runGetOrFail getDNSType input of
+        Left (_,_,err) -> putStrLn $ "解析失败: " ++ err
+        Right (remaining,_,typ) -> do 
+          putStrLn $ "1解析成功:" ++ show typ
+          parseAllType remaining
 
 main :: IO ()
 main = do
-    let input = "\x00\x01"
-    case runGetOrFail getDNSType (BS.pack input) of
-        Left (_,_,err) -> putStrLn $ "解析失败: " ++ err
-        Right (_,_,typ) -> putStrLn $ "解析成功: " ++ show typ
+    let input = "\x00\x01\x00\x02\x00\x03\x00\x05\x00\x06"
+    parseAllType (BS.pack input)
+          
