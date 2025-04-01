b750f927dcc962a472ef8a041ac2a3e535eeba8f
zhanglei <huli115386945@126.com>
2025-04-02 00:01:16 +0800

    fix bug,but failed

--- src/Types.hs
+++ src/Types.hs
@@ -4,10 +4,35 @@ module Types where
 
 import Data.Word (Word8, Word16, Word32)
 import Data.ByteString (ByteString)
-import Data.IP
+import Data.IP (IPv4, IPv6)
+import Data.Bits (shiftR, (.&.))
 import qualified Data.ByteString.Char8 as BS
-import Data.Binary (Binary(..))
+import Data.Binary (Binary(..), putWord8, getWord8)
+import Data.Binary.Put (putWord16be)
+import Data.Binary.Get (getWord16be)
 import GHC.Generics (Generic)
+import Data.IP (toIPv4w, toIPv6w)
+import Data.Bits (shiftR, (.&.))
+import Data.IP (IPv4, IPv6, toIPv4w, toIPv6w)
+import Control.Monad (replicateM)
+
+toOctets :: IP -> [Word8]
+toOctets (IPv4 addr) = let (a, b, c, d) = toIPv4w addr in [fromIntegral a, fromIntegral b, fromIntegral c, fromIntegral d]
+toOctets _ = error "Not an IPv4 address"
+
+toWord16s :: IP -> [Word16]
+toWord16s (IPv6 addr) = let (a, b, c, d, e, f, g, h) = toIPv6w addr in [a, b, c, d, e, f, g, h]
+toWord16s _ = error "Not an IPv6 address"
+
+instance Binary IPv4 where
+    put ip = mapM_ putWord8 (toOctets (IPv4 ip))
+    get = toIPv4 <$> (replicateM 4 getWord8 >>= \ws -> return (fromIntegral <$> ws))
+
+instance Binary IPv6 where
+    put ip = mapM_ putWord16be (toWord16s (IPv6 ip))
+    get = toIPv6 <$> (replicateM 8 getWord16be >>= \ws -> return (fromIntegral <$> ws))
+
+
 
 data DNSType
     = A
@@ -19,7 +44,31 @@ data DNSType
     | TXT
     | AAAA
     | Other Word16
-    deriving (Show, Eq)
+    deriving (Show, Eq, Generic)
+
+instance Binary DNSType where
+    put A = putWord8 1
+    put NS = putWord8 2
+    put CNAME = putWord8 5
+    put SOA = putWord8 6
+    put PTR = putWord8 12
+    put MX = putWord8 15
+    put TXT = putWord8 16
+    put AAAA = putWord8 28
+    put (Other code) = putWord16be code
+
+    get = do
+        tag <- getWord8
+        case tag of
+            1 -> return A
+            2 -> return NS
+            5 -> return CNAME
+            6 -> return SOA
+            12 -> return PTR
+            15 -> return MX
+            16 -> return TXT
+            28 -> return AAAA
+            _ -> Other <$> getWord16be
 
 data DNSClass
     = IN
@@ -27,7 +76,23 @@ data DNSClass
     | CH
     | HS
     | OtherClass Word16
-    deriving (Show, Eq)
+    deriving (Show, Eq, Generic)
+
+instance Binary DNSClass where
+    put IN = putWord16be 1
+    put CS = putWord16be 2
+    put CH = putWord16be 3
+    put HS = putWord16be 4
+    put (OtherClass code) = putWord16be code
+
+    get = do
+        tag <- getWord16be
+        case tag of
+            1 -> return IN
+            2 -> return CS
+            3 -> return CH
+            4 -> return HS
+            _ -> OtherClass <$> getWord16be
 
 data DNSRecord = DNSRecord 
     { recordName :: ByteString
@@ -35,7 +100,16 @@ data DNSRecord = DNSRecord
     , recordClass :: DNSClass
     , recordTTL :: Word32
     , recordData :: DNSRData
-    } deriving (Show, Eq)
+    } deriving (Show, Eq, Generic)
+
+instance Binary DNSRecord where
+    put (DNSRecord name typ cls ttl rdata) = do
+        put name
+        put typ
+        put cls
+        put ttl
+        put rdata
+    get = DNSRecord <$> get <*> get <*> get <*> get <*> get
 
 data DNSRData
     = ARecord IPv4
@@ -47,7 +121,31 @@ data DNSRData
     | SOARecord SOAData
     | PTRRecord ByteString
     | UnknownRecord ByteString
-    deriving (Show, Eq)
+    deriving (Show, Eq, Generic)
+
+instance Binary DNSRData where
+    put (ARecord ipv4) = putWord8 1 >> put ipv4
+    put (AAAARecord ipv6) = putWord8 2 >> put ipv6
+    put (CNAMERecord bs) = putWord8 3 >> put bs
+    put (NSRecord bs) = putWord8 4 >> put bs
+    put (MXRecord priority bs) = putWord8 5 >> put priority >> put bs
+    put (TXTRecord bs) = putWord8 6 >> put bs
+    put (SOARecord soa) = putWord8 7 >> put soa
+    put (PTRRecord bs) = putWord8 8 >> put bs
+    put (UnknownRecord bs) = putWord8 9 >> put bs
+
+    get = do
+        tag <- getWord8
+        case tag of
+            1 -> ARecord <$> get
+            2 -> AAAARecord <$> get
+            3 -> CNAMERecord <$> get
+            4 -> NSRecord <$> get
+            5 -> MXRecord <$> get <*> get
+            6 -> TXTRecord <$> get
+            7 -> SOARecord <$> get
+            8 -> PTRRecord <$> get
+            _ -> UnknownRecord <$> get
 
 data SOAData = SOAData
     { soaMName :: ByteString
@@ -57,7 +155,18 @@ data SOAData = SOAData
     , soaRetry :: Word32
     , soaExpire :: Word32
     , soaMinimum :: Word32
-    } deriving (Show, Eq)
+    } deriving (Show, Eq, Generic)
+
+instance Binary SOAData where
+    put (SOAData mName rName serial refresh retry expire minimum) = do
+        put mName
+        put rName
+        put serial
+        put refresh
+        put retry
+        put expire
+        put minimum
+    get = SOAData <$> get <*> get <*> get <*> get <*> get <*> get <*> get
 
 data DNSHeader = DNSHeader
     { headerId :: Word16
